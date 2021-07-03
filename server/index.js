a782a0e145f5efdd6e29d111c04abcbad12b7d1c
Vibhu Gautam <vibhuparmar13@gmail.com>
2021-07-03 20:15:09 +0530

    Changes made in Cors

--- server/index.js
+++ server/index.js
@@ -8,12 +8,14 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts' , postRoutes);
 
 
 app.use(bodyParser.json({limit: "30mb" , extended:  true}));
 app.use(bodyParser.urlencoded({limit: "30mb" , extended:  true}));
 app.use(cors());
+
+
+app.use('/posts' , postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://vibhu:vibhu_123@memory.qgros.mongodb.net/memory?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
