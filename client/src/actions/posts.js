9ab83f488d0b9c204a5412ad48c75b73850d1918
Vibhu Gautam <vibhuparmar13@gmail.com>
2021-07-03 20:16:22 +0530

    Action Creaters

---
+++ client/src/actions/posts.js
@@ -0,0 +1,31 @@
+import * as api from '../api';
+
+
+
+// Action Creaters
+
+export const getPosts = () => async (dispatch) => {
+
+    try {
+        const {data} = await api.fetchPosts();  
+        
+        dispatch({ type : 'FETCH_ALL' , payload: data })
+    } catch (error) {
+        console.log(error.message);
+    }
+
+    const action = { type : 'FETCH_ALL' , payload: []}
+
+    dispatch(action);
+}
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+        const { data } = await api.createPost(post);
+
+        dispatch({ type : 'CREATE', payload: data });
+
+    } catch (e) {
+        console.log(e);
+    }
+}
