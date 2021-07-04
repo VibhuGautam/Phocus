2afd26185cd82513d137656aa142df305ac2f9f0
Vibhu Gautam <vibhuparmar13@gmail.com>
2021-07-04 13:07:04 +0530

    DB Hidden

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
-
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 
 
 app.use(bodyParser.json({limit: "30mb" , extended:  true}));
@@ -17,11 +17,14 @@ app.use(cors());
 
 app.use('/posts' , postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://vibhu:vibhu_123@memory.qgros.mongodb.net/memory?retryWrites=true&w=majority'
+app.get('/' , (req, res) => {
+    res.send('Hello to memories API');
+}) 
+
 const PORT = process.env.PORT || 5000;
 
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true , useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true , useUnifiedTopology: true })
     .then(() => app.listen(PORT , () => console.log(`Server running on port : ${PORT}`)))
     .catch((error) => console.log(error.message));
 
