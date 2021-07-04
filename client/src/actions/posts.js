fb70c72a9c3af20ee4a1d7a7832664cf459e1bb0
Vibhu Gautam <vibhuparmar13@gmail.com>
2021-07-04 10:49:05 +0530

    Editing and Creating Memory

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -29,3 +29,14 @@ export const createPost = (post) => async (dispatch) => {
         console.log(e);
     }
 }
+
+export const updatePost = (id , post) => async (dispatch) => {
+    try {
+        const { data } = await api.updatePost(id , post);
+
+        dispatch({type : 'UPDATE' , payload: data});
+
+    } catch (e) {
+        console.log(e);
+    } 
+}
