9f7397a4a7aed8d5b255f9d2b157fd1750adf9b2
Vibhu Gautam <vibhuparmar13@gmail.com>
2021-07-06 19:55:23 +0530

    Submit working

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -7,24 +7,37 @@ import LockOutlinedIcon from '@material-ui/icons/LockOpenOutlined';
 import useStyles from './styles';
 import Input from './Input';
 import Icon from './icon';
+import { signin , signup } from '../../actions/auth';
 
+
+const initialState = { firstName: '', lastName: '', email: '', password: '', confirmPassword: '' };
 
 const Auth = () => {
-    const classes = useStyles();
+    const classes = useStyles(initialState);
 
     const [showPassword , setShowPassword] = useState(false);
     const [ isSignup , setIsSignup ] = useState(false);
+    const [ formData , setFormData ] = useState();
+
     const dispatch = useDispatch();
     const history = useHistory();
     
     const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
 
-    }
+        e.preventDefault();
+    
+        if (isSignup) {
+          dispatch(signup(formData, history));
+        } 
+        else {
+          dispatch(signin(formData, history));
+        }
+    };
 
-    const handleChange = () => {
-
+    const handleChange = (e) => {
+        setFormData({...formData , [e.target.name]: e.target.value })
     }
 
     const switchMode = () => {
@@ -33,7 +46,9 @@ const Auth = () => {
     }
 
     const googleSuccess = async (res) => {
+
         // console.log(res);
+
         const result = res?.profileObj;
         const token = res?.tokenId;
     
@@ -63,7 +78,7 @@ const Auth = () => {
                             isSignup && (
                                 <>
                                     <Input name = "firstname" label = "First Name" handleChange =  {handleChange} autoFocus half />
-                                    <Input name = "firstname" label = "First Name" handleChange =  {handleChange} half  />
+                                    <Input name = "lastname" label = "Last Name" handleChange =  {handleChange} half  />
                                 </>
                             )
                         }
