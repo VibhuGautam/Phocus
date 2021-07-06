aa13c3e121b1792f8fb6753c24ae2b938966e4e0
Vibhu Gautam <vibhuparmar13@gmail.com>
2021-07-06 19:56:10 +0530

    Updated

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import cors from 'cors'
 import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users`.js';
+
 
 const app = express();
 dotenv.config();
@@ -16,6 +18,7 @@ app.use(cors());
 
 
 app.use('/posts' , postRoutes);
+app.use('/user', userRoutes);
 
 app.get('/' , (req, res) => {
     res.send('Hello to memories API');
