174a30826a80fd27e6eb6d9d23aa9d96f705719f
Vibhu Gautam <vibhuparmar13@gmail.com>
2021-07-06 11:53:14 +0530

    Logout Functionality

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Avatar , AppBar , Typography , Toolbar , Button } from '@material-ui/core';
-import {Link} from 'react-router-dom';
+import {Link, useHistory , useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import useStyles from './styles';
 import memories from '../../images/memories.png'
 
@@ -8,7 +9,32 @@ const Navbar = () => {
 
     const classes = useStyles();
 
-    const user = null;
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const location = useLocation();
+
+    
+    const logout = () => {
+        dispatch({ type: actionType.LOGOUT });
+    
+        history.push('/auth');
+    
+        setUser(null);
+    };
+
+
+    useEffect(() => {
+        const token = user?.token;
+    
+        if (token) {
+          const decodedToken = decode(token);
+    
+          if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+        }
+    
+        setUser(JSON.parse(localStorage.getItem('profile')));
+      }, [location]);
 
     return (
         <AppBar className = {classes.appBar} position = "static" color = "inherit">
@@ -27,7 +53,7 @@ const Navbar = () => {
                         <Typography className =  {classes.userName} variant = "h6">
                             {user.result.name}
                         </Typography>
-                        <Button variant = "contained" className = {classes.logout} color = "secondary">
+                        <Button variant = "contained" className = {classes.logout} color = "secondary" onClick={logout}>
                             Logout
                         </Button>
                     </div>
