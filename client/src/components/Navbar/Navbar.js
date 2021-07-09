9f8c5a3d5dde82754c5b6a4227933a36db4c2df3
Vibhu Gautam <vibhuparmar13@gmail.com>
2021-07-09 22:27:48 +0530

    Link Component Added

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -43,20 +43,17 @@ const Navbar = () => {
     return (
         <AppBar className = {classes.appBar} position = "static" color = "inherit">
             <Link to = "/" className = {classes.brandContainer}>
-                {/* <Typography component = {Link} to = "/" className = {classes.heading} variant = "h3" align = "center">
-                    Memories
-                </Typography> */}
-                <img src = {memoriesText} alt = "icon" height = "45px" />
-                <img className ={classes.image} src = {memoriesLogo} alt = "icon" height = "40px" />        
+            <img component={Link} to="/" src={memoriesText} alt="icon" height="45px" />
+            <img className={classes.image} src={memoriesLogo} alt="icon" height="40px" />        
             </Link>
             <Toolbar className = {classes.toolbar}>
-                {user ? (
+                {user?.result ? (
                     <div className = {classes.profile}>
-                        <Avatar className = {classes.purple} alt = {user.result.name} src = {user.result.imageUrl}>
-                            {user.result.name.charAt(0)}
+                        <Avatar className = {classes.purple} alt = {user?.result.name} src = {user?.result.imageUrl}>
+                            {user?.result.name.charAt(0)}
                         </Avatar>
                         <Typography className =  {classes.userName} variant = "h6">
-                            {user.result.name}
+                            {user?.result.name}
                         </Typography>
                         <Button variant = "contained" className = {classes.logout} color = "secondary" onClick={logout}>
                             Logout
