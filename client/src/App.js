9e3b3d59d6ed3b0dfd30e7daff5a27946a5002c2
Vibhu Gautam <vibhuparmar13@gmail.com>
2021-07-09 22:25:38 +0530

    CreatorOrTag Added

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import PostDetails from './components/PostDetails/PostDetails';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
-
+import CreatorOrTag from './components/CreatorOrTag/CreatorOrTag';
 
 
 const App = () => {
@@ -23,6 +23,7 @@ const App = () => {
             <Route path="/posts" exact component={Home} />
             <Route path="/posts/search" exact component={Home} />
             <Route path="/posts/:id" exact component={PostDetails} />
+            <Route path={['/creators/:name', '/tags/:name']} component={CreatorOrTag} />
             <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
             </Switch>
         </Container>
