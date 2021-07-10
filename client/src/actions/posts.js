383d57e6769f38036b3c7821826d6c2e71dfa18e
Vibhu Gautam <vibhuparmar13@gmail.com>
2021-07-10 15:45:47 +0530

    Updated

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,13 +1,14 @@
-import { START_LOADING, END_LOADING, FETCH_ALL, FETCH_POST, FETCH_BY_SEARCH , FETCH_BY_CREATOR , CREATE, UPDATE, DELETE, LIKE, COMMENT } from '../constants/actionType'
+import { START_LOADING, END_LOADING, FETCH_ALL, FETCH_POST, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE, LIKE, COMMENT, FETCH_BY_CREATOR } from '../constants/actionTypes';
 import * as api from '../api/index.js';
 
 export const getPost = (id) => async (dispatch) => {
   try {
     dispatch({ type: START_LOADING });
-    const { data } = await api.fetchPosts(id);
 
-    dispatch({ type: FETCH_POST, payload: data });
-    dispatch({ type: END_LOADING });
+    const { data } = await api.fetchPost(id);
+
+    dispatch({ type: FETCH_POST, payload: { post: data } });
+    
   } catch (error) {
     console.log(error);
   }
@@ -40,7 +41,7 @@ export const getPostsByCreator = (name) => async (dispatch) => {
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
   try {
     dispatch({ type: START_LOADING });
-    const { data : { data } } = await api.fetchPostsBySearch(searchQuery);
+    const { data: { data } } = await api.fetchPostsBySearch(searchQuery);
 
     dispatch({ type: FETCH_BY_SEARCH, payload: { data } });
     dispatch({ type: END_LOADING });
