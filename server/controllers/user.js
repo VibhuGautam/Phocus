be6514f8ba56e61f5e32ff8b2642d90f2724811f
Vibhu Gautam <vibhuparmar13@gmail.com>
2021-07-10 15:42:53 +0530

    UserModal updated

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-import User from "../models/user.js";
+import UserModal from "../models/user.js";
 
 const secret = 'test';
 
@@ -9,39 +9,34 @@ export const signin = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const oldUser = await User.findOne({ email });
+    const oldUser = await UserModal.findOne({ email });
 
     if (!oldUser) 
-        return res.status(404).json({ message: "User doesn't exist" }); 
+      return res.status(404).json({ message: "User doesn't exist" });
 
     const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
 
-    if (!isPasswordCorrect) 
-      return res.status(400).json({ message: "Invalid credentials" });
+    if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
 
     const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, { expiresIn: "1h" });
 
     res.status(200).json({ result: oldUser, token });
   } catch (err) {
     res.status(500).json({ message: "Something went wrong" });
-  } 
+  }
 };
 
 export const signup = async (req, res) => {
-  const { email, password, confirmPassword , firstName, lastName } = req.body;
+  const { email, password, firstName, lastName } = req.body;
 
   try {
-    const oldUser = await User.findOne({ email });
+    const oldUser = await UserModal.findOne({ email });
 
-    if (oldUser) 
-        return res.status(400).json({ message: "User already exists" });
-
-    if(password !== confirmPassword)
-      return res.status(400).json({ message: "Password do not match"});
+    if (oldUser) return res.status(400).json({ message: "User already exists" });
 
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
+    const result = await UserModal.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
 
     const token = jwt.sign( { email: result.email, id: result._id }, secret, { expiresIn: "1h" } );
 
