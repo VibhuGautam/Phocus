3ea6ae2f2a6c8673c85d49f8b5d606dee2f219a7
Vibhu Gautam <vibhuparmar13@gmail.com>
2021-07-12 10:51:55 +0530

    No Post added

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -11,14 +11,16 @@ const Posts = ({ setCurrentId }) => {
     const classes = useStyles();
 
     if( !posts.length && !isLoading)
-        return 'No Posts';
+        return (
+          <div className = {classes.nopost}>No Post</div>
+        );
 
   
     return (
         isLoading ? <CircularProgress /> : (      
         <Grid className={classes.container} container alignItems="stretch" spacing={3}>
             {posts?.map((post) => (
-              <Grid key={post._id} item xs={12} sm={12} md={6} lg={3}>
+              <Grid key={post._id} item xs={12} sm={12} md={6} lg={4}>
                 <Post post={post} setCurrentId={setCurrentId} />
               </Grid>
             ))}
